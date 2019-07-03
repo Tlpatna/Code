703402156d1ccf0836c59c6f9597fd7d98843adc
Tlpatna <52210006+Tlpatna@users.noreply.github.com>
2019-07-03 12:07:42 +0700

    Create Exchange.cpp

---
+++ Exchange.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+void main()
+{
+	/*float bahts;
+cout << "Enter your money :";
+cin >> bahts;
+cout << "Exchange bahts to dollar :" << (bahts * 0.03256) << endl;
+cout << "Exchange bahts to Euto   :" << (bahts * 0.02856) << endl;
+cout << "Exchange bahts to Yen    :" << bahts * 3.52 << endl;
+cout << "Exchange bahts to Dong   :" << bahts * 760.33 <<endl;
+cout << "Exchange bahts to Ponds  :" << bahts *0.026 << endl;
+
+	int l,w;
+cout <<"Enter Length :";
+cin>> l;
+cout <<"Enter Widht :";
+cin>>w;
+cout<<"Area of Rectagle = " << l*w <<endl ;
+
+//Program Calculate Area Circle.
+#define pi 3.1415
+float r;
+cout<<"Circle radius (real number) ";
+cin>>r;
+cout<<"Area of circle with radius "<<r<<" is "<<pi*r*r<<endl;
+cout<<"Circumference is "<<2*pi*r<<endl;
+
+	double x;
+	cout <<"Enter value X :";
+	cin >>x;
+	double result;
+	result = pow(2,2*x+1) + 12*pow(2,x)+16;
+	cout<<"Result  =";
+	cout<<result<<endl;
+
+	int A,B;
+	cout<<"Enter first number(A): ";
+	cin>>A;
+	cout<<"Enter second number(B): ";
+	cin>>B;
+	cout<<"A || B = " << (A||B) << endl;
+
+	int num;
+		cout<<"Enter number :";
+	cin>>num;
+	cout<<num<<" is " <<(num > 60 ? "Fat ": "Slender")<<endl;*/
+}
